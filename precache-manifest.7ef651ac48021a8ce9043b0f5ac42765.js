74dd1a903f501984f47a56768fea1e1cc1347972
peterjast <peterjast@yahoo.com>
2019-05-02 06:08:39 -0700

    Updates

---
+++ precache-manifest.7ef651ac48021a8ce9043b0f5ac42765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7c224dfca8ab1b0e42884e95a02ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4931039a5c50995f7f60",
+    "url": "/static/css/main.2872a665.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4931039a5c50995f7f60",
+    "url": "/static/js/main.b2dffea2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
